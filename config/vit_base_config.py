0d0c19247ed4512b44e783657e39a2bf6167110b
lchu <lchu@us.ibm.com>
2023-03-31 14:14:39 -0400

    up

--- config/vit_base_config.py
+++ config/vit_base_config.py
@@ -165,7 +165,7 @@ def build_model(model_size: str, layernorm_eps_in: float = 1e-6):
             "c_stem_strides": [],
             "c_stem_dims": [],
             "n_layers": 48,
-            "n_heads": 32,
+            "n_heads": 48,
             "hidden_d": 7680,
             "mlp_d": 30720,
             "cls_type": "token",
