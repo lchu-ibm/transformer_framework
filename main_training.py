8bc866f4c0abb92446e3fdd2bcc49f3d9b4057e6
lchu <lchu@us.ibm.com>
2023-04-19 13:42:10 -0400

    30B with 10

--- main_training.py
+++ main_training.py
@@ -373,7 +373,7 @@ def fsdp_main():
         )
         rank_print(rank, f"{twod_mesh=}")
 
-        for i in range(48):
+        for i in range(40):
             block = model.get_submodule(f"encoder.block_{i}")
             parallelized_block = parallelize_module(
                 module=block,
