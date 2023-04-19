c68527b5165fcef0e41abd9fdd94b2621ec4530e
lchu <lchu@us.ibm.com>
2023-04-19 14:34:57 -0400

    28b with 70

--- config/vit_base_config.py
+++ config/vit_base_config.py
@@ -43,7 +43,7 @@ class train_config(base_config):
     use_tp: bool = True
 
     # training
-    batch_size_training: int = 40
+    batch_size_training: int = 70
 
     # image size
     image_size: int = 224
